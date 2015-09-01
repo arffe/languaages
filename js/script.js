ed85c327e426a340b0c8b2f8a7534c39154966a6
Chris Harris <chrisharris@caradoc.co.uk>
2015-09-01 20:38:37 +0100

    first commit of js files

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/*
+this is a test
+javascript programme
+*/
+/*function myFunction1 () {
+     switch ( grade ) {
+     case "Regular":
+        alert("its' $3.35");
+        break;
+     case "Premium":
+        alert("its' $3.75");
+        break;
+     case "Diesel":
+        alert("its' $3.40");
+        break;
+     default:
+     alert("that's not a valid grade");
+   }
+}
+*/
+function myFunction2(a,b) {
+  var result = a + b;
+  return(result);
+}
+var x = myFunction2(5,10);
+alert( "the result is x= " +   x );
