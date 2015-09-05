45e2d974404c72c2213c3722c9c93c84be941851
Chris Harris <chrisharris@caradoc.co.uk>
2015-09-05 13:43:47 +0100

    more changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
 /*
-this is a test
-javascript programme
+this is a test javascript programme
 */
-/*function myFunction1 () {
+var y = Math.PI;
+alert(y);
+
+/*
+var mydate = new Date();
+//mydate.setFullYear(2015,8,5);
+alert(mydate);
+var x = 23.45;
+alert(x);
+var y = Math.round(x);
+alert(y);
+*/
+/*
+var myword = prompt("enter word you wish to check");
+var fred = "We want a groovy keyword";
+//var fred = "We want a groovy keyword”;
+if (fred.indexOf(myword) == -1) {
+	alert("\"" + myword + "\" does not occur");
+}
+else {
+	alert("position is " + fred.indexOf(myword));
+}
+
+
+function myFunction1 () {
      switch ( grade ) {
      case "Regular":
         alert("its' $3.35");
@@ -17,10 +40,30 @@ javascript programme
      alert("that's not a valid grade");
    }
 }
-*/
+
 function myFunction2(a,b) {
   var result = a + b;
   return(result);
 }
 var x = myFunction2(5,10);
 alert( "the result is x= " +   x );
+
+
+var a = 1;
+while (a < 10) {
+	alert(a);
+	a++;
+}
+
+var amount = 0;
+for (i=1 ; i<=10 ; i++) {
+	amount += 100;
+	alert(amount);
+}
+*/
+
+
+
+
+
+
