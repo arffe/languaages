764d3ae68fefbafd4d8e274358130e28fc81a5ae
Chris Harris <chrisharris@caradoc.co.uk>
2015-09-11 05:59:51 +0100

    Clock from pi mag

---
+++ python/clock.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import time,pygame
+pygame.init()
+theFont=pygame.font.Font(None,72)
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode([320, 200])
+pygame.display.set_caption('pi time')
+while True:
+	clock.tick(1)
+	theTime=time.strftime("%H:%M:%S",time.localtime())
+	timeText=theFont.render(str(theTime), True,(255,255,255),(0,0,0))
+	screen.blit(timeText, (80,60))
+	pygame.display.update()
