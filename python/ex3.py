17c88f4b393735c6d473acfaf241d946ad0adb47
Chris Harris <chrisharris@caradoc.co.uk>
2015-09-19 17:32:16 +0100

    python the hard way exercises

---
+++ python/ex3.py
@@ -0,0 +1,24 @@
+# simple math
+print "Hens", 25 + 30 /6
+# % = modulus
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now will I count my eggs:"
+
+print 3 + 2 + 1 -5 + 4 % 2 - 1 / 4 + 6
+
+print "is it true that 3 + 2  < 5 - 77?"
+# comparison operator retrurns a boolean value
+print 3+2 < 5-7 
+
+print "what is 3 + 2?", 3 + 2
+print "what is 5 - 7?", 5 -77 
+
+print "Oh, that's why it's false."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+print "floating point:", 5.3 * 6
---
+++ python/ex5.py
@@ -0,0 +1,20 @@
+# more variable
+name = "Chris"
+age = 21 # a lie
+height = 73 # inches
+weight = 183 # lbs
+eyes = 'Blue'
+teeth = 'white'
+hair = 'grey'
+
+# conversion
+metric_height = height * 2.54
+metric_weight = weight * 0.453
+
+print "Let's talk about %s." % name
+print "He's %d inches tall, that's %d cm." % (height, metric_height)
+print "He's %d pounds heavy, that's %d kg." % (weight, metric_weight)
+print "Actually that's not too heavy." 
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+print "If I add %d, %d and %d, I get %d." % ( age, height, weight, age + height + weight)
