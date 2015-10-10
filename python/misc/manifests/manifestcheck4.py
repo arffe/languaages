a16d1a77298bdad7cf4eba1045498197298f9ac4
chris <chrisharris@caradoc.co.uk>
2015-10-10 21:47:29 +0100

    changes

--- python/misc/manifests/manifestcheck4.py
+++ python/misc/manifests/manifestcheck4.py
@@ -7,14 +7,22 @@ from xml.etree import ElementTree
 def main():
     policy=["normal","feedbackgroup","feedbackgroup2","feedbackgroup3","feedbackgroup4","feedbackgroup5"]
     variant=["80B0.7000","84B0.7000","80B0.8500","84B0.8500","86B0.8500","87B0.8500"]
-    print " "
-    print "+----------+----------+----------+----------+----------+----------+----------+"
-    print "| variant  |  normal  |    fbg   |   fbg 2  |   fbg 3  |   fbg 4  |   fbg 5  |"
-    print "+----------+----------+----------+----------+----------+----------+----------+"
+    
+    printpol = []
+    cdslen=8
+    for v in policy:
+	printpol.append(v.replace("feedbackgroup", "fbg "))
+    print "\n"
+    print "+----------" * (len(policy) + 1) + "+"
+    print "| variant ",
+    for y in printpol:
+        print "| {txt: ^{width}}".format(txt=y,width=cdslen), 
+    print "|"
+    print "+----------" * (len(policy) + 1) + "+"
     for vnt in variant:
         cdslist = []
         cleanvar = vnt.replace(".", "")
-        cdslist.append(cleanvar)
+        #cdslist.append(cleanvar)
         for grp in policy:
             URL = "URL&SystemID={}&SWVersion=8.4.20&policy={}.xml".format(vnt,grp)
             with open(URL, 'rt') as f:
@@ -22,8 +30,11 @@ def main():
             for node in tree.iter("version"):
                 x = node.text
             cdslist.append(x)
-        print '|', ' | '.join([str(item) for item in cdslist]), '|'
-    print "+----------+----------+----------+----------+----------+----------+----------+"
+        print "| {}".format(cleanvar),
+	for p in cdslist:
+	    print "| {txt: ^{width}}".format(txt=p,width=cdslen),
+	print "|"
+    print "+----------" * (len(policy) + 1) + "+"
     print " "
 
 if __name__ == "__main__":
---
+++ python/misc/manifests/manifestcheck5.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+# manifest check
+
+from xml.etree import ElementTree
+import time
+
+def main():
+    policy=["normal","feedbackgroup","feedbackgroup2","feedbackgroup3","feedbackgroup4","feedbackgroup5"]
+    variant=["80B0.7000","84B0.7000","80B0.8500","84B0.8500","86B0.8500","87B0.8500"]
+
+# ADD warning re manifest and conmfiormation check 
+# ADD whatist to onlycheck normal hosting
+
+# initialise variables
+    cdslen=0
+    version_table=[]
+# get versions
+    for vnt in variant:
+        verlist = []
+        for grp in policy:
+	    # ADD time check to throttle requests
+            URL = "URL&SystemID={}&SWVersion=8.4.20&policy={}.xml".format(vnt,grp)
+            with open(URL, 'rt') as f:
+                tree = ElementTree.parse(f)
+            # ADD error checking for no file etc
+            for node in tree.iter("version"):
+                x = node.text
+            if len(x) > cdslen:
+		cdslen = len(x)
+            verlist.append(x)
+        version_table.append(verlist)
+   
+   # print table
+    print "\n+----------" + ("+" + "-" * (cdslen+2)) * (len(policy)) + "+"
+    print "| variant  |",
+    for pol in policy:
+	shortpol = pol.replace("feedbackgroup", "fbg ")
+        print " {txt: ^{width}}|".format(txt=shortpol,width=cdslen),
+    print  "\n+----------" + ("+" + "-" * (cdslen+2)) * (len(policy)) + "+",
+    for variant_data in version_table:
+	a = version_table.index(variant_data)
+	cleanvar = variant[a].replace(".", "")
+	print "\n| " + cleanvar + " |",
+	for cds_version in variant_data:
+	    print "{txt: ^{width}} |".format(txt=cds_version,width=cdslen),
+    print  "\n+----------" + ("+" + "-" * (cdslen+2)) * (len(policy)) + "+\n"
+      
+if __name__ == "__main__":
+    main();
