36c70ef69f86fc1c86b035eaa871152b846be0a3
Chris Harris <chrisharris@caradoc.co.uk>
2015-10-10 07:53:14 +0100

    changes

---
+++ python/graphics.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import pygame, sys
+import math
+pygame.init()
+screen = pygame.display.set_mode([640,480])
+screen.fill([0,0,0])
+plotPoints = []
+for x in range(0,640):
+    y = int(math.sin(x/640.0 * 4 * math.pi) * 200 + 240)
+    plotPoints.append([x, y])
+pygame.draw.lines(screen, [255,0,0], False, plotPoints, 1)
+pygame.display.flip()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
